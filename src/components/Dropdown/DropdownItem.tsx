2e80c44e046fcecc9a3e7b7b091b534305dd17c0
Waheed Uddin Ahmed <dmc4719@gmail.com>
2023-06-25 00:50:16 +0600

    Add filter dropdown

--- src/components/Dropdown/DropdownItem.tsx
+++ src/components/Dropdown/DropdownItem.tsx
@@ -7,7 +7,7 @@ type Props<T> = {
   handleItemChange: (value: T) => void
 }
 
-const DropdownItem = <T extends ReactNode | { id: number; value: string }>({
+const DropdownItem = <T extends ReactNode>({
   selectedOption,
   value,
   handleItemChange,
